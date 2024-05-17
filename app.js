ee0ae3208737b218ec18594b2e2c5b0fe8afc431
Ksenia51 <Tesa11234455666t@yandex.ru>
2024-05-17 21:16:06 +0300

    feat: all games, GET request

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const gamesRouter = require("./routes/games");
-const categoriesRoutes = require("./routes/categories");
+//const categoriesRoutes = ("./routes/categories") 
 const usersRouter = require("./routes/users");
-const connectToDatabase = require("");
+const connectToDatabase = require("./database/connect");
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 connectToDatabase();
@@ -17,7 +17,6 @@ app.use(
     express.static(path.join(__dirname, "public")),
     usersRouter,
     gamesRouter,
-    categoriesRoutes
 );
 
 app.listen(PORT, () => {
---
+++ moduls/games.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const gameSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    developer: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    },
+});
+
+const games = mongoose.model("games", gameSchema);
+
+module.exports = games;
