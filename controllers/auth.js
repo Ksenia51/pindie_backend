bb17f0a57645d91677849e385e231ec9ad3b7d04
Ksenia51 <Tesa11234455666t@yandex.ru>
2024-05-25 18:37:43 +0300

    feat: added authorization router

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,17 @@
 const users = require("../moduls/users");
-const bcrypt = require("bcryptjs");
 
-const login = (res, req) => {
-    const {email, password} = req.body;
-    users.findOne({ email }).then((user) => {
-        if (!user) {
-            return Promise.reject(new Error("Неправильная почта или пароль"));
-        }
-        return bcrypt.compare(password, user.password).then ((matched) => {
-            if (!matched) {
-                return Promise.reject(new Error("Неправильная почта или пароль"));
-            }
-            return user;
-        });
-    })
+const login = (req, res) => {
+  const { email, password } = req.body;
+  users
+    .findUserByCredentials(email, password)
     .then((user) => {
-        res
+      res
         .status(200)
         .send({ _id: user._id, username: user.username, email: user.email });
     })
     .catch((error) => {
-        re.status(401).send({ message: error.massege });
+      re.status(401).send({ message: error.massege });
     });
 };
 
-    module.exports = { login };
+module.exports = { login };
--- moduls/users.js
+++ moduls/users.js
@@ -1,20 +1,35 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error("Неправильная почта или пароль"));
+    }
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Неправильная почта или пароль"));
+      }
+      return user;
+    });
+  });
+};
 
 const users = mongoose.model("users", userSchema);
 
